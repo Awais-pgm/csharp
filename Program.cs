148ca8389a46fe0c22288a46d6f1d46e2f9c5b8a
Muhammad Awais <80377676+Awais-pgm@users.noreply.github.com>
2021-04-01 00:30:20 -0700

    Added

--- Program.cs
+++ Program.cs
@@ -1,63 +1,31 @@
-﻿using System;
+﻿using System.IO;
+using System;
 
-namespace Question_59
+namespace question_50
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Part a
-            /*
-            int n=22/8;
-            Console.WriteLine(n);
-            */
-            //Part b
-            /*
-            int n=99/8+21/11*7;
-            Console.WriteLine(n);
-            */
+            //conerting the cpp expressions to c#
+            // question_50 part a
+        //Console.WriteLine(x*x+4*x-4);
+            //part b
+        //Console.WriteLine{(x+y)*x);
             //part c
-            /*
-            double a=4, b=3,p=8, q=2;
-            double n=q/a-p/b;
-            Console.WriteLine(n); 
-            */
+        //Console.WriteLine((x+3*y)/(2*x-y));
             //part d
-            /*
-            int a=6, b=3, p=8, q=2;
-            double n=p/a+q/b;
-            Console.WriteLine(n);
-            */
-            //part e
-            /*
-            int a=(int) 3.3, b=(int)2.7,p=(int)8.7,q=(int)5.4;
-            double n=p/b+q/a;
-            Console.WriteLine(n);
-            */
-            //Part f
-            /*
-            int n=11+7/4+98%6*3;
-            Console.WriteLine(n);
-            */
+        // Console.WriteLine(1/(x*x+x+3));
+            //part f
+        // Console.WriteLine(2*b*c*c*c);
             //part g
-            /*
-            int n=11/31%8*5-12;
-            Console.WriteLine(n);
-            */
-            //Part h
-            /*
-            double p=8;
-            int q=5;
-            int n= (int)(q*p+3.0*p*p-(q%3)*p*p*p);
-            Console.WriteLine(n);
-            */
-            //Part i
-            /*
-            double p=5;
-            int q=9;
-            int n=(int)((q%2)*p+6.0*p*(q%3)+(q%4)*p*p*p);
-            Console.WriteLine(n);
-            */
+        // Console.WriteLine((3*y)/(5-z));
+            //part h
+        // Console.WriteLine(area*sqrt(area));
+            // part i
+        // Console.WriteLine((x+32)/(y-32)-(x-2*y));
+            //part j
+        // Console.WriteLine((3*int*j*k+pow(k,9))/(7*i*k-5*sqrt(j+k)));
         }
     }
 }
