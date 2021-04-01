4cf13fbfae397a9c68276de1e4c65a879b9d34ea
Muhammad Awais <80377676+Awais-pgm@users.noreply.github.com>
2021-04-01 00:36:18 -0700

    Added

---
+++ Question 55/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Question_55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //part a
+            /*
+            int a=3,b=4;
+            a=a+1;
+            a*=b++;
+            Console.WriteLine(a);
+            */
+            //Part b
+            /*
+            int a=3,b=4;
+            a=a+1;
+            a*=++b;
+            Console.WriteLine(a);
+            */
+        }
+    }
+}
